05b17a155692afc0ba239875847825f940c063bc
Nathan <nrfowler@gmail.com>
2016-09-15 16:54:45 -0500

    get rid of guy fieri

---
+++ static/routes.js
@@ -0,0 +1,35 @@
+/* @ngInject */
+angular.
+  module('webpack-app').
+  config(['$routeProvider',
+    function config($routeProvider) {
+        console.log("hello console")
+      $routeProvider.
+      	when('/home', {
+
+          templateUrl: './user/userTemplate.html',
+          controller: 'HomeController',
+          controllerAs: 'homeController'
+
+        }).
+      	when('/user', {
+
+          templateUrl: 'http://localhost:8080/#/' + 'user/userTemplate.html',
+          controller: 'UserController',
+          controllerAs: 'userController',
+          resolve: {
+              allUsers: function(UserService){
+	                  return UserService.getAllUsers();
+	          }
+          }
+
+        }).
+        when('/#user/:id', {
+
+        	templateUrl: './user/userTemplate.html',
+        	controller: 'UserDetailController',
+        	controllerAs: 'userDetailController'
+        }).
+        otherwise('/home');
+    }
+  ]);
